e0dc46dc400ab75a3667d799b50a3f8f4b43fe04
Mikel Severson <mkseve@gmail.com>
2016-01-22 20:32:15 -0600

    tested middleware in api router, and experiment with 404 error on dist file lookup

--- server/api/router.js
+++ server/api/router.js
@@ -1,5 +1,21 @@
 var router = require('express').Router();
 
+// This may be a great place to add middleware for API requests
+// I would like any API request to require auth so I am hoping I can
+// throw an error when this point is accessed without auth
+
+
+router.use((req, res, next) => {
+	console.log('hit');
+	next();
+});
+
 router.use('/test', require('./test/testRoutes'));
+
+//api catchall so it doesn't move on to frontend requests 
+//the public router will look for a file in dist/api/* without this
+router.get("/*", function(req, res) {
+    res.send('Not found');
+});
 
 module.exports = router;
--- server/public/router.js
+++ server/public/router.js
@@ -2,10 +2,17 @@ var router = require('express').Router();
 var path = require('path');
 var config = require('../config/config');
 
+//Routes
+
+
 //dist catch-all
 router.get("/*", function(req, res) {
     var file = req.params[0] || config.dist.defaultFile;
-    res.sendFile(path.join(__dirname, "../public/dist", file));
+    res.sendFile(path.join(__dirname, "../public/dist", file), function(err) {
+    	if (err) {
+    		res.send("404");
+    	}
+    });
 });
 
 module.exports = router;
