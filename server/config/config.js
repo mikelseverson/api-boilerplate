75c44a102170b0ee0005bdcbb0003e3fb5af8f09
Mikel Severson <mkseve@gmail.com>
2016-01-22 20:33:33 -0600

    clarified try catch reasoning

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,9 @@ var config = {
 process.env.NODE_ENV = process.env.NODE_ENV || config.dev;
 config.env = process.env.NODE_ENV;
 
-//This needs improvements, but I believe it's required incase there is a missing file for the active enviroment
+//This needs improvements, 
+//but I believe it's required incase there is a missing file for the active enviroment
+//when merging, but it may not be required
 var envConfig;
 try {
   envConfig = require('./' + config.env);
